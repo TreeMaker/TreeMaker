4c0abb457332ff115fc921a008aef2a81233f3e8
ahmad <ahmad_borzou@baylor.edu>
2015-07-09 12:05:33 -0500

    Hadronic Tau added

---
+++ AllHadronicSUSY/TreeMaker/python/hadtau_treeMaker.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+HadTau_TreeMaker = cms.EDProducer(
+'HadTau_TreeMaker',
+# Name of the output tree
+TreeName          = cms.string('RA2Tree'),
+## might help if something isn working wilil produce couts
+debug = cms.bool(False),
+# list of reco candidate objects eg leptons: For each reco cand collection the pt eta phi e and Tlorentzvector will be stored in arrays. In addition optional float int or bool variables can be stored
+# syntax example:  VarsRecoCand = cms.vstring('selectedIDIsoMuons(selectedIDIsoMuonsName)|Isolation(F_NameOfIsolationInTreeisolation)|Jup(b_nameofJup)|wwww(F)','selectedIDMuons','selectedRecoIsoElec','selectedRecoElec'),
+# selectedIDIsoMuons would be the tag of the reco candiate 
+# (selectedIDIsoMuonsName ) optoinal naming in the tree if not defined tag name will be used
+# separated with | addition variables int float or bool defined by: TagName(typ) always needed optional TagName(typ_NameInTree) 
+VarsTLoretzVector = cms.vstring(),
+VarsString = cms.vstring(),
+VarsDouble = cms.vstring(),
+VarsInt = cms.vstring(),
+VarsBool = cms.vstring(),
+VarsRecoCand = cms.vstring(),
+VectorDouble = cms.vstring(),
+VectorInt = cms.vstring(),
+VectorBool = cms.vstring(),
+VectorString = cms.vstring(),
+VectorTLorentzVector = cms.vstring(),
+)
--- AllHadronicSUSY/TreeMaker/test/runMakeTreeFromMiniAOD_cfg.py
+++ AllHadronicSUSY/TreeMaker/test/runMakeTreeFromMiniAOD_cfg.py
@@ -1,10 +1,28 @@
 # Read parameters
 from AllHadronicSUSY.Utils.CommandLineParams import CommandLineParams
 parameters = CommandLineParams()
+
 #dataSetName = parameters.value("dataset","file:/pnfs/desy.de/cms/tier2/store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/F452BBD7-BE76-E411-B1D7-002590DB928E.root")
 dataSetName = parameters.value("dataset","/store/mc/Phys14DR/DYJetsToLL_M-50_HT-600toInf_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/04860BAA-B673-E411-8B20-002481E0D50C.root")
 global_tag = parameters.value("global_tag","")
-lostlepton= parameters.value("lostlepton", False)
+
+###################################
+## For hadronic tau -> turn this on
+###################################
+TauHad_ = False 
+if TauHad_:
+    #dataSetName = parameters.value("dataset","file:/data3/store/user/borzou/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14dr-PU20bx25_POSTLS170_V5AN1-miniAOD706p1/141014_155652/0000/miniAOD-prod_PAT_9.root")
+    #dataSetName = parameters.value("dataset","/store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/00C90EFC-3074-E411-A845-002590DB9262.root")
+    #dataSetName = parameters.value("dataset","/store/mc/Phys14DR/SMS-T1tttt_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/10000/4836AA58-5A6B-E411-8865-20CF305B053E.root")
+    dataSetName = parameters.value("dataset","/store/mc/Phys14DR/SMS-T1tttt_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/10000/829D372D-7F6B-E411-81B1-0025907B5048.root")
+    #dataSetName = parameters.value("dataset","/store/mc/Phys14DR/SMS-T1tttt_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/10000/C43D68C5-8D6B-E411-B030-0025907750A0.root")
+
+    global_tag = parameters.value("global_tag","PHYS14_25_V1::All") # should be consistent with dataSetName
+######
+# End 
+######
+
+lostlepton= parameters.value("lostlepton", True)
 gammajets= parameters.value("gammajets", False)
 tagandprobe= parameters.value("tagandprobe", False)
 applybaseline= parameters.value("applybaseline", False)
@@ -32,7 +50,8 @@ makeTreeFromMiniAOD(process,
   reportEveryEvt=5000,
   testFileName=dataSetName,
   Global_Tag=global_tag,
-  numProcessedEvt=-1,
+  numProcessedEvt=1000,
+  TauHad=TauHad_,
   lostlepton=lostlepton,
   gammajets=gammajets,
   tagandprobe=tagandprobe,
