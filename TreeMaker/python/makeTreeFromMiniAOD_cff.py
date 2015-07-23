b0ab33aadc55099bbad00bab819545c29483a3ae
Kevin Pedro <kpedro88@gmail.com>
2015-07-23 14:10:58 -0500

    fix weight producer to use elif and handle vstrings

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -104,7 +104,7 @@ applyjec=False,
     ## WeightProducer
     ## ----------------------------------------------------------------------------------------------
     from TreeMaker.WeightProducer.getWeightProducer_cff import getWeightProducer
-    process.WeightProducer = getWeightProducer(dataset)
+    process.WeightProducer = getWeightProducer(process.source.fileNames[0])
     process.WeightProducer.Lumi                       = cms.double(4000)
     process.WeightProducer.PU                         = cms.int32(0) # PU S10 3 for S10 2 for S7
     process.WeightProducer.FileNamePUDataDistribution = cms.string("NONE")
