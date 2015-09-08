1ef35407bf076f3fb34e015472a90b4b09be4c56
aminghiasi <sghia001@ucr.edu>
2015-09-08 00:27:55 -0700

    minDeltaPhiQCD

--- Utils/python/deltaphiqcd_cfi.py
+++ Utils/python/deltaphiqcd_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-deltaphiqcd  = cms.EDProducer('DeltaPhiQCD',
-JetTagRecoJets = cms.InputTag('GoodJets'),
-JetTagGenJets = cms.InputTag('slimmedGenJets'),
-BTagInputTag = cms.InputTag('combinedInclusiveSecondaryVertexV2BJetTags'),
-GenParticleTag = cms.InputTag('prunedGenParticles'),
-GenMETTag  = cms.InputTag('slimmedMETs')
+deltaphiqcd  = cms.EDProducer ( 'DeltaPhiQCD',
+JetTagRecoJets = cms.InputTag ( 'GoodJets' ) ,
+JetTagGenJets = cms.InputTag ( 'slimmedGenJets' ) ,
+BTagInputTag = cms.InputTag ( 'combinedInclusiveSecondaryVertexV2BJetTags' ) ,
+GenParticleTag = cms.InputTag ( 'prunedGenParticles' ) ,
+GenMETTag  = cms.InputTag ( 'slimmedMETs' )
 )
 
