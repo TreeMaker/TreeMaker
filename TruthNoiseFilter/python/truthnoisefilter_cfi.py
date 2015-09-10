e8dff2d06768c2c864426251c80a9b6c68512322
Kevin Pedro <kpedro88@gmail.com>
2015-09-10 09:50:27 -0500

    remove non-shared code directories

--- TruthNoiseFilter/python/truthnoisefilter_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-TruthNoiseFilter = cms.EDFilter('TruthNoiseFilter',
-                                genjetCollection = cms.InputTag('slimmedGenJets'),
-                                jetCollection = cms.InputTag('slimmedJets'),
-                                recoJetPt = cms.double(100.),
-                                genJetPt = cms.double(20.),
-                                dRmax = cms.double(0.05)
-                                )
