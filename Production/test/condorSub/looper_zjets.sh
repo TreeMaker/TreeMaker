79d60a7cda2ad2f3e652562c05ae7ee6d4a69396
Kevin Pedro <kpedro88@gmail.com>
2016-07-05 18:41:27 -0500

    add some more gjet samples

--- Production/test/condorSub/looper_zjets.sh
+++ Production/test/condorSub/looper_zjets.sh
@@ -42,10 +42,13 @@ Spring16.GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
 Spring16.GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
 Spring16.GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
 Spring16.GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
+Spring16.GJets_DR-0p4_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
+Spring16.GJets_DR-0p4_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
+Spring16.GJets_DR-0p4_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
+Spring16.GJets_DR-0p4_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
 )
 
 #missing:
-#Spring16.DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8 \
 #Spring16.ZJetsToNuNu_HT-400To600_13TeV-madgraph \
 
 for SAMPLE in ${SAMPLES[@]}; do
