3f0b6ba45008dee6e54ff22b88bc761a1bd3b363
sambein <samuel.bein@gmail.com>
2016-10-13 18:05:46 -0500

    deleted two unused parameters

--- Utils/python/prescaleweightproducer_cfi.py
+++ Utils/python/prescaleweightproducer_cfi.py
@@ -2,10 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 prescaleweightProducer = cms.EDProducer('PrescaleWeightProducer',
 
-   HLTProcess = cms.string("HLT"),
-
-   PFHTWeights = cms.bool( True ),
-
    bits = cms.InputTag("TriggerResults","","HLT"),
 
    prescales = cms.InputTag("patTrigger"),
