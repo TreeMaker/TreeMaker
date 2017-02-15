b85d5801c377570e461f6105d5850d98a8beefac
Kevin Pedro <kpedro88@gmail.com>
2017-02-14 23:45:24 -0600

    get HTMHT G

--- Production/test/condorSub/dict_re2016G.py
+++ Production/test/condorSub/dict_re2016G.py
@@ -1,7 +1,7 @@
 flist = {
     "scenario": "2016ReMiniAOD03Feb",
     "samples": [
-#        ['Run2016G-03Feb2017-v1.HTMHT'],
+        ['Run2016G-03Feb2017-v1.HTMHT'],
         ['Run2016G-03Feb2017-v1.JetHT'],
         ['Run2016G-03Feb2017-v1.MET'],
         ['Run2016G-03Feb2017-v1.SingleElectron'],
