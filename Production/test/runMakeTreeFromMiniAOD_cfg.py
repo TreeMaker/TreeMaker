69e62e7ad2aa39bf3018a59a968e2a6599c1d2f8
Kevin Pedro <kpedro88@gmail.com>
2017-08-10 15:56:15 -0500

    add multithread run options

--- Production/test/runMakeTreeFromMiniAOD_cfg.py
+++ Production/test/runMakeTreeFromMiniAOD_cfg.py
@@ -13,6 +13,8 @@ reportfreq=parameters.value("reportfreq",1000)
 outfile=parameters.value("outfile","test_run")
 dump=parameters.value("dump",False)
 mp=parameters.value("mp",False)
+threads=parameters.value("threads",1)
+streams=parameters.value("streams",0)
 
 # background estimations on by default
 lostlepton=parameters.value("lostlepton", True)
@@ -106,6 +108,10 @@ if len(jecfile)>0: print " JECs applied: "+jecfile+(" (residuals)" if residual e
 if len(jerfile)>0: print " JERs applied: "+jerfile
 if len(pufile)>0: print " PU weights stored: "+pufile
 print " era of this dataset: "+era
+if mp: print " running memory profile"
+if threads>1:
+    print " threads: "+str(threads)
+    if streams>0: print " streams: "+str(streams)
 print "************************************************"
 
 from TreeMaker.TreeMaker.makeTreeFromMiniAOD_cff import makeTreeFromMiniAOD
@@ -146,6 +152,10 @@ if mp:
         reportToFileAtPostEvent = cms.untracked.string('| gzip -c > '+outfile+'___memory___%I.gz')
     )
 
+# multithreading options
+if threads>1:
+    process.options.numberOfThreads = cms.untracked.uint32(threads)
+    process.options.numberOfStreams = cms.untracked.uint32(streams if streams>0 else 0)
 
 # if requested, dump and exit
 if dump:
