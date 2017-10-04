924dd7ff9826327cf18e6fe0949b8bdc32886732
Kevin Pedro <kpedro88@gmail.com>
2017-10-04 17:34:12 -0500

    semivisible jet samples

---
+++ Production/test/condorSub/dict_svj1.py
@@ -0,0 +1,8 @@
+flist = {
+    "scenario": "Summer16sig",
+    "samples": [
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p1_n-500'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p2_n-500'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p5_alpha-0p2_n-500'],
+    ]
+}
