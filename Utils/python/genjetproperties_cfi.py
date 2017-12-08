5a092dce0ea192b7f122f88623205b4826a7b38f
Kevin Pedro <kpedro88@gmail.com>
2017-12-08 16:37:05 -0600

    store ak8 gen jets and properties

--- Utils/python/genjetproperties_cfi.py
+++ Utils/python/genjetproperties_cfi.py
@@ -1,5 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 genjetproperties = cms.EDProducer('GenJetProperties',
-GenJetTag               = cms.InputTag('GenMHTJets')
+    GenJetTag = cms.InputTag('GenMHTJets'),
+    PrunedGenJetTag = cms.InputTag(""),
+    SoftDropGenJetTag = cms.InputTag(""),
+    distMax = cms.double(0.4),
 )
