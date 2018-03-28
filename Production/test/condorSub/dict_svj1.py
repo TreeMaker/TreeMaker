f3f7371b3c17c83cc4fa042803adabfec995ccd8
Kevin Pedro <kpedro88@gmail.com>
2018-03-28 04:46:59 -0500

    update svj dict

--- Production/test/condorSub/dict_svj1.py
+++
@@ -1,8 +0,0 @@
-flist = {
-    "scenario": "Summer16sig",
-    "samples": [
-        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p1_n-500'],
-        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p2_n-500'],
-        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p5_alpha-0p2_n-500'],
-    ]
-}
