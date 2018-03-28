14e0d96df825c62e34baf20bad8aa1726d79c836
Kevin Pedro <kpedro88@gmail.com>
2018-03-28 04:46:59 -0500

    update svj dict

---
+++ Production/test/condorSub/dict_svj.py
@@ -0,0 +1,18 @@
+flist = {
+    "scenario": "Summer16sig",
+    "samples": [
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p1_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p5_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-1000_mDark-20_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-2000_mDark-20_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-4000_mDark-20_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-1_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-50_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-100_rinv-0p3_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p1_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p7_alpha-0p2_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-0p5_n-1000'],
+        ['PrivateSamples.SVJ_mZprime-3000_mDark-20_rinv-0p3_alpha-1_n-1000']
+    ]
+}
