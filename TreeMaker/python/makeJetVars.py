770f629110c6cd0eefcd85c7fbe09ad69f4fd138
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-04-11 08:45:57 -0500

    Add localera based change to GoodJetsProducer configuration. Also have makeGoodJets be jet cone size aware. Add some new jet ID constraints for 2017.

--- TreeMaker/python/makeJetVars.py
+++ TreeMaker/python/makeJetVars.py
@@ -30,27 +30,54 @@ def makeMHTVars(self, process, JetTag, HTJetsTag, storeProperties, suff, MHTsuff
     
     return process
 
-def makeGoodJets(self, process, JetTag, suff, storeProperties, SkipTag=cms.VInputTag()):
+def makeGoodJets(self, process, JetTag, suff, storeProperties, SkipTag=cms.VInputTag(), jetConeSize=0.4):
     from TreeMaker.Utils.goodjetsproducer_cfi import GoodJetsProducer
-    GoodJets = GoodJetsProducer.clone(
-        TagMode                   = cms.bool(True),
-        JetTag                    = JetTag,
-        maxJetEta                 = cms.double(5.0),
-        minNconstituents          = cms.int32(1),
-        minNneutrals              = cms.int32(10),
-        minNcharged               = cms.int32(0),
-        maxNeutralFraction        = cms.double(0.99),
-        maxPhotonFraction         = cms.double(0.99),
-        maxPhotonFractionHF       = cms.double(0.90),
-        minChargedFraction        = cms.double(0),
-        maxChargedEMFraction      = cms.double(0.99),
-        jetPtFilter               = cms.double(30),
-        ExcludeLepIsoTrackPhotons = cms.bool(True),
-        JetConeSize               = cms.double(0.4),
-        SkipTag                   = SkipTag,
-        SaveAllJetsId             = True,
-        SaveAllJetsPt             = False, # exclude low pt jets from good collection
-    )
+    if self.localera == 'TM2016':
+        GoodJets = GoodJetsProducer.clone(
+            TagMode                   = cms.bool(True),
+            JetTag                    = JetTag,
+            maxJetEta                 = cms.double(5.0),
+            minNconstituents          = cms.int32(1),
+            minNneutrals              = cms.int32(10),
+            minNcharged               = cms.int32(0),
+            maxNeutralFraction        = cms.double(0.99),
+            maxPhotonFraction         = cms.double(0.99),
+            maxPhotonFractionHF       = cms.double(0.90),
+            minChargedFraction        = cms.double(0),
+            maxChargedEMFraction      = cms.double(0.99),
+            jetPtFilter               = cms.double(30),
+            ExcludeLepIsoTrackPhotons = cms.bool(True),
+            JetConeSize               = cms.double(jetConeSize),
+            SkipTag                   = SkipTag,
+            SaveAllJetsId             = True,
+            SaveAllJetsPt             = False, # exclude low pt jets from good collection
+        )
+    elif self.localera == 'TM2017':
+        GoodJets = GoodJetsProducer.clone(
+            TagMode                   = cms.bool(True),
+            JetTag                    = JetTag,
+            maxJetEta                 = cms.double(5.0),
+            minNconstituents          = cms.int32(1),
+            minNneutrals              = cms.int32(10),
+            minNcharged               = cms.int32(0),
+            maxNeutralFraction        = cms.double(0.90),
+            maxNeutralFractionHE      = cms.double(1.0), #Turned off as not needed for the tight WP
+            minNeutralFractionHF      = cms.double(0.02),
+            maxPhotonFraction         = cms.double(0.90),
+            minPhotonFractionHE       = cms.double(0.02),
+            maxPhotonFractionHE       = cms.double(0.99),
+            maxPhotonFractionHF       = cms.double(0.90),
+            minChargedFraction        = cms.double(0),
+            maxChargedEMFraction      = cms.double(1.0), #Turned off as not needed for the tight WP
+            jetPtFilter               = cms.double(170 if jetConeSize==0.8 else 30),
+            ExcludeLepIsoTrackPhotons = cms.bool(True),
+            JetConeSize               = cms.double(jetConeSize),
+            SkipTag                   = SkipTag,
+            SaveAllJetsId             = True,
+            SaveAllJetsPt             = False, # exclude low pt jets from good collection
+        )
+    else:
+        GoodJets = GoodJetsProducer.clone()
     if self.fastsim: GoodJets.jetPtFilter = cms.double(20)
     setattr(process,"GoodJets"+suff,GoodJets)
     GoodJetsTag = cms.InputTag("GoodJets"+suff)
@@ -69,7 +96,7 @@ def makeJetVars(self, process, JetTag, suff, skipGoodJets, storeProperties, Skip
     if skipGoodJets:
         GoodJetsTag = JetTag
     else:
-        process, GoodJetsTag = self.makeGoodJets(process,JetTag,suff,storeProperties,SkipTag)
+        process, GoodJetsTag = self.makeGoodJets(process,JetTag,suff,storeProperties,SkipTag,0.4)
         if onlyGoodJets:
             return process
     
@@ -150,12 +177,30 @@ def makeJetVars(self, process, JetTag, suff, skipGoodJets, storeProperties, Skip
         )
         # provide extra info where necessary
         if storeProperties==1: 
-            JetProperties.properties = cms.vstring("bDiscriminatorCSV","muonEnergyFraction","chargedHadronEnergyFraction","partonFlavor","hadronFlavor")
+            JetProperties.properties = cms.vstring("bDiscriminatorCSV","bJetTagDeepCSVprobb","bJetTagDeepCSVprobc",
+                                                   "bJetTagDeepCSVprobudsg","bJetTagDeepCSVprobbb","bDiscriminatorDeepCSVBvsAll",
+                                                   "bDiscriminatorDeepCSVCvsB","bDiscriminatorDeepCSVCvsL","bJetTagDeepFlavourprobb",
+                                                   "bJetTagDeepFlavourprobc","bJetTagDeepFlavourprobg","bJetTagDeepFlavourproblepb",
+                                                   "bJetTagDeepFlavourprobbb","bJetTagDeepFlavourprobuds","muonEnergyFraction",
+                                                   "chargedHadronEnergyFraction","partonFlavor","hadronFlavor")
         if storeProperties>1 and self.geninfo:
             JetProperties.properties.extend(["jerFactor", "jerFactorUp","jerFactorDown"])
         setattr(process,"JetProperties"+suff,JetProperties)
         self.VectorDouble.extend([
             'JetProperties'+suff+':bDiscriminatorCSV(Jets'+suff+'_bDiscriminatorCSV)',
+            'JetProperties'+suff+':bJetTagDeepCSVprobb(Jets'+suff+'_bJetTagDeepCSVprobb)',
+            'JetProperties'+suff+':bJetTagDeepCSVprobc(Jets'+suff+'_bJetTagDeepCSVprobc)',
+            'JetProperties'+suff+':bJetTagDeepCSVprobudsg(Jets'+suff+'_bJetTagDeepCSVprobudsg)',
+            'JetProperties'+suff+':bJetTagDeepCSVprobbb(Jets'+suff+'_bJetTagDeepCSVprobbb)',
+            'JetProperties'+suff+':bDiscriminatorDeepCSVBvsAll(Jets'+suff+'_bJetTagDeepCSVBvsAll)',
+            'JetProperties'+suff+':bDiscriminatorDeepCSVCvsB(Jets'+suff+'_bJetTagDeepCSVCvsB)',
+            'JetProperties'+suff+':bDiscriminatorDeepCSVCvsL(Jets'+suff+'_bJetTagDeepCSVCvsL)',
+            'JetProperties'+suff+':bJetTagDeepFlavourprobb(Jets'+suff+'_bJetTagDeepFlavourprobb)',
+            'JetProperties'+suff+':bJetTagDeepFlavourprobc(Jets'+suff+'_bJetTagDeepFlavourprobc)',
+            'JetProperties'+suff+':bJetTagDeepFlavourprobg(Jets'+suff+'_bJetTagDeepFlavourprobg)',
+            'JetProperties'+suff+':bJetTagDeepFlavourproblepb(Jets'+suff+'_bJetTagDeepFlavourproblepb)',
+            'JetProperties'+suff+':bJetTagDeepFlavourprobbb(Jets'+suff+'_bJetTagDeepFlavourprobbb)',
+            'JetProperties'+suff+':bJetTagDeepFlavourprobuds(Jets'+suff+'_bJetTagDeepFlavourprobuds)',
             'JetProperties'+suff+':muonEnergyFraction(Jets'+suff+'_muonEnergyFraction)',
             'JetProperties'+suff+':chargedHadronEnergyFraction(Jets'+suff+'_chargedHadronEnergyFraction)',
         ])
@@ -221,7 +266,7 @@ def makeJetVarsAK8(self, process, JetTag, suff, storeProperties):
         # add discriminator and update tag
         process, JetTag = addJetInfo(process, JetTag, ak8floats, ak8ints)
 
-    process, GoodJetsTag = self.makeGoodJets(process,JetTag,suff,storeProperties)
+    process, GoodJetsTag = self.makeGoodJets(process,JetTag,suff,storeProperties,jetConeSize=0.8)
 
     if storeProperties>0:
         # AK8 jet variables - separate instance of jet properties producer
@@ -229,29 +274,41 @@ def makeJetVarsAK8(self, process, JetTag, suff, storeProperties):
         JetPropertiesAK8 = jetproperties.clone(
             JetTag       = GoodJetsTag,
             properties = cms.vstring(
-                "prunedMass"           ,
-                "softDropMass"         ,
-                "NsubjettinessTau1"    ,
-                "NsubjettinessTau2"    ,
-                "NsubjettinessTau3"    ,
-                "bDiscriminatorCSV"    ,
-                "NumBhadrons"          ,
-                "NumChadrons"          ,
-                "subjets"              ,
+                "prunedMass"            ,
+                "softDropMass"          ,
+                "NsubjettinessTau1"     ,
+                "NsubjettinessTau2"     ,
+                "NsubjettinessTau3"     ,
+                "bDiscriminatorCSV"     ,
+                "bJetTagDeepCSVprobb"   ,
+                "bJetTagDeepCSVprobc"   ,
+                "bJetTagDeepCSVprobudsg",
+                "bJetTagDeepCSVprobbb"  ,
+                "NumBhadrons"           ,
+                "NumChadrons"           ,
+                "subjets"               ,
             )
         )
         # specify userfloats
-        JetPropertiesAK8.prunedMass = cms.vstring('ak8PFJetsCHSPrunedMass')
-        JetPropertiesAK8.softDropMass = cms.vstring('SoftDrop') # computed from subjets
-        JetPropertiesAK8.NsubjettinessTau1 = cms.vstring('NjettinessAK8:tau1')
-        JetPropertiesAK8.NsubjettinessTau2 = cms.vstring('NjettinessAK8:tau2')
-        JetPropertiesAK8.NsubjettinessTau3 = cms.vstring('NjettinessAK8:tau3')
+        JetPropertiesAK8.prunedMass = cms.vstring('ak8PFJetsCHSValueMap:ak8PFJetsCHSPrunedMass')
+        JetPropertiesAK8.softDropMass = cms.vstring('SoftDropPuppi') # computed from subjets
+        JetPropertiesAK8.NsubjettinessTau1 = cms.vstring('NjettinessAK8Puppi:tau1')
+        JetPropertiesAK8.NsubjettinessTau2 = cms.vstring('NjettinessAK8Puppi:tau2')
+        JetPropertiesAK8.NsubjettinessTau3 = cms.vstring('NjettinessAK8Puppi:tau3')
         JetPropertiesAK8.bDiscriminatorCSV = cms.vstring('pfBoostedDoubleSecondaryVertexAK8BJetTags')
-        JetPropertiesAK8.subjets = cms.vstring('SoftDrop')
+        JetPropertiesAK8.bJetTagDeepCSVprobb = cms.vstring('pfDeepCSVJetTags:probb')
+        JetPropertiesAK8.bJetTagDeepCSVprobc = cms.vstring('pfDeepCSVJetTags:probc')
+        JetPropertiesAK8.bJetTagDeepCSVprobudsg = cms.vstring('pfDeepCSVJetTags:probudsg')
+        JetPropertiesAK8.bJetTagDeepCSVprobbb = cms.vstring('pfDeepCSVJetTags:probbb')
+        JetPropertiesAK8.subjets = cms.vstring('SoftDropPuppi')
         self.VectorDouble.extend([
                              'JetProperties'+suff+':prunedMass(Jets'+suff+'_prunedMass)',
                              'JetProperties'+suff+':softDropMass(Jets'+suff+'_softDropMass)',
                              'JetProperties'+suff+':bDiscriminatorCSV(Jets'+suff+'_doubleBDiscriminator)',
+                             'JetProperties'+suff+':bJetTagDeepCSVprobb(Jets'+suff+'_bJetTagDeepCSVprobb)',
+                             'JetProperties'+suff+':bJetTagDeepCSVprobc(Jets'+suff+'_bJetTagDeepCSVprobc)',
+                             'JetProperties'+suff+':bJetTagDeepCSVprobudsg(Jets'+suff+'_bJetTagDeepCSVprobudsg)',
+                             'JetProperties'+suff+':bJetTagDeepCSVprobbb(Jets'+suff+'_bJetTagDeepCSVprobbb)',
                              'JetProperties'+suff+':NsubjettinessTau1(Jets'+suff+'_NsubjettinessTau1)',
                              'JetProperties'+suff+':NsubjettinessTau2(Jets'+suff+'_NsubjettinessTau2)',
                              'JetProperties'+suff+':NsubjettinessTau3(Jets'+suff+'_NsubjettinessTau3)'])
