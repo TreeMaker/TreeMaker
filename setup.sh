f939e879a5142115f9a1360bf917b59a529d8065
Kevin Pedro <kpedro88@gmail.com>
2018-06-22 16:03:26 -0500

    annotate sections

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ BRANCH=Run2_2017
 ACCESS=ssh
 CORES=8
 
+# process options
 while getopts "f:b:a:j:" opt; do
 	case "$opt" in
 	f) FORK=$OPTARG
@@ -18,6 +19,7 @@ while getopts "f:b:a:j:" opt; do
 	esac
 done
 
+# check options
 if [ "$ACCESS" = "ssh" ]; then
 	ACCESS_GITHUB=git@github.com:
 	ACCESS_GITLAB=ssh://git@gitlab.cern.ch:7999/
@@ -28,6 +30,7 @@ else
 	exit 1
 fi
 
+# get CMSSW release
 export SCRAM_ARCH=slc6_amd64_gcc630
 # cmsrel
 CMSSWVER=CMSSW_9_4_8
@@ -38,12 +41,14 @@ eval `scramv1 runtime -sh`
 git cms-init
 git config gc.auto 0
 
+# DeepAK8 setup
 if [ "$ACCESS" = "https" ]; then echo "Needs your CERN username and password: NNKit is being cloned from gitlab"; fi
 git clone ${ACCESS_GITLAB}DeepAK8/NNKit.git
 cp NNKit/misc/mxnet_predict.xml $CMSSW_BASE/config/toolbox/$SCRAM_ARCH/tools/selected
 scram setup mxnet_predict
 cp NNKit/misc/lib/libmxnet_predict.so $CMSSW_BASE/external/$SCRAM_ARCH/lib/libmxnet_predict.so
 
+# CMSSW patches
 git cms-merge-topic TreeMaker:JERFormula942 # this one has dependencies (will be included in next 94X)
 git cms-merge-topic -u TreeMaker:BoostedDoubleSVTaggerV4-WithWeightFiles-v1_from-CMSSW_9_4_2
 git cms-merge-topic -u TreeMaker:MET_942_FixEGdR # only for 2016 re-miniAOD MET egamma fix
@@ -52,12 +57,15 @@ git cms-merge-topic -u TreeMaker:AddJetAxis1_942
 git cms-merge-topic -u TreeMaker:NjettinessAxis_942
 git cms-merge-topic -u TreeMaker:SpeedupPuppi948 # will be included in next 94X
 
+# outside repositories
 git clone ${ACCESS_GITHUB}TreeMaker/JetToolbox.git JMEAnalysis/JetToolbox -b jetToolbox_94X
 git clone ${ACCESS_GITHUB}kpedro88/CondorProduction.git Condor/Production
 git clone ${ACCESS_GITHUB}${FORK}/TreeMaker.git -b ${BRANCH}
 
+# compile
 scram b -j ${CORES}
 
+# extra setup
 cd TreeMaker/Production/test/condorSub/
 python $CMSSW_BASE/src/Condor/Production/python/linkScripts.py
 ln -s ${CMSSW_BASE}/src/NNKit/data/ak8/full/preprocessing.json ${CMSSW_BASE}/src/TreeMaker/Production/test/data/.
