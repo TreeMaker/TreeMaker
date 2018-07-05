6fa9188391aa239907286fa274ff0939c6aa63fe
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-07-05 09:26:33 -0500

    Add the 2018 condorSub dictionaries.

---
+++ Production/test/condorSub/dict_2018A.py
@@ -0,0 +1,17 @@
+flist = {
+    "scenario": "2018Prompt",
+    "samples": [
+        ['Run2018A-PromptReco-v1.EGamma'],
+        ['Run2018A-PromptReco-v1.JetHT'],
+        ['Run2018A-PromptReco-v1.MET'],
+        ['Run2018A-PromptReco-v1.SingleMuon'],
+        ['Run2018A-PromptReco-v2.EGamma'],
+        ['Run2018A-PromptReco-v2.JetHT'],
+        ['Run2018A-PromptReco-v2.MET'],
+        ['Run2018A-PromptReco-v2.SingleMuon'],
+        ['Run2018A-PromptReco-v3.EGamma'],
+        ['Run2018A-PromptReco-v3.JetHT'],
+        ['Run2018A-PromptReco-v3.MET'],
+        ['Run2018A-PromptReco-v3.SingleMuon'],
+    ]
+}
