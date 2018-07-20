3f25745c4d43c5c9f0e800a5cc700d5f23777169
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-07-20 10:32:24 -0500

    Add the dictionaries needed to calculate the luminosity for the 2018 V15 and V15a productions. Also rename the original dataSamples.py dictionary to indicate that it includes 2016 data samples.

---
+++ Production/test/dataSamples_Run2018B_RelValHEMiss.py
@@ -0,0 +1,12 @@
+dataSamples = [
+    # 2018B datasets: prompt reco
+    [ "EGamma_2018B-RelVal",         "Run2018B-RelVal-v1.EGamma_*"] ,
+    [ "JetHT_2018B-RelVal",          "Run2018B-RelVal-v1.JetHT_*"] ,
+    [ "MET_2018B-RelVal",            "Run2018B-RelVal-v1.MET_*"] ,
+    [ "SingleMuon_2018B-RelVal",     "Run2018B-RelVal-v1.SingleMuon_*"] ,
+    # 2018BHEMiss datasets: prompt reco
+    [ "EGamma_2018B-RelVal-HEMiss",         "Run2018B-RelVal-HEMiss-v1.EGamma_*"] ,
+    [ "JetHT_2018B-RelVal-HEMiss",          "Run2018B-RelVal-HEMiss-v1.JetHT_*"] ,
+    [ "MET_2018B-RelVal-HEMiss",            "Run2018B-RelVal-HEMiss-v1.MET_*"] ,
+    [ "SingleMuon_2018B-RelVal-HEMiss",     "Run2018B-RelVal-HEMiss-v1.SingleMuon_*"] ,
+]
