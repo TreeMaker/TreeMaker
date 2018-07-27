bd1d651b2b011b6655ab4e095a64350e21c17b06
Kevin Pedro <kpedro88@gmail.com>
2018-07-27 18:05:11 -0500

    add met sig speedup

--- setup.sh
+++ setup.sh
@@ -70,6 +70,7 @@ git cms-merge-topic -u TreeMaker:storeJERFactorIndex942
 git cms-merge-topic -u TreeMaker:AddJetAxis1_942
 git cms-merge-topic -u TreeMaker:NjettinessAxis_948
 git cms-merge-topic -u TreeMaker:METFixEE2017_949
+git cms-merge-topic -u TreeMaker:SpeedupMETSig_949
 
 # outside repositories
 git clone ${ACCESS_GITHUB}TreeMaker/JetToolbox.git JMEAnalysis/JetToolbox -b jetToolbox_94X
