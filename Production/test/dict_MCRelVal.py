4bef4b5a07ca03b86a17915cbd6bdc119ea9098b
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-08-08 09:41:19 -0500

    Prepare to process the larger TTbar (semileptonic) samples for the HEM analysis

--- Production/test/dict_MCRelVal.py
+++ Production/test/dict_MCRelVal.py
@@ -6,12 +6,13 @@
 # MC w/ negative weights (amcatnlo) + extended sample: [False, ['sample','sample_ext'] , [neff, neff_ext]]
 
 flist = [
-[False, ['/RelValTTbar_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValTTbar_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValSMS-T1tttt_mGl-1500_mLSP-100_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValSMS-T1tttt_mGl-1500_mLSP-100_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValQCD_Pt_80_120_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValQCD_Pt_600_800_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
-[False, ['/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValTTbar_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValTTbar_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValSMS-T1tttt_mGl-1500_mLSP-100_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValSMS-T1tttt_mGl-1500_mLSP-100_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValQCD_Pt_80_120_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValQCD_Pt_600_800_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_1_1-101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+#[False, ['/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_1_1-PU25ns_101X_upgrade2018_realistic_v7-v1/MINIAODSIM'] , []],
+[False, ['/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISpring18MiniAOD-100X_upgrade2018_realistic_v10_ext1-v3/MINIAODSIM'] , []],
 ]
