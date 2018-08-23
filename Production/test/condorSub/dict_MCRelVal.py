dbe0e137fd9255b585d82eb1fc0cc72ff0d5b0b6
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-08-23 16:15:24 -0500

    Add the dictionaries needed to make condor jobs for the high stats HEM production. Also update the RelVal sample locations because of the folder name change in #340.

--- Production/test/condorSub/dict_MCRelVal.py
+++ Production/test/condorSub/dict_MCRelVal.py
@@ -1,10 +1,10 @@
 flist = {
     "scenario": "RelValCMSSW101X",
     "samples": [
-        ['MCRelVal.RelValTTbar_13'],
-        ['MCRelVal.RelValQCD_FlatPt_15_3000HS_13'],
-        ['MCRelVal.RelValQCD_Pt_80_120_13'],
-        ['MCRelVal.RelValQCD_Pt_600_800_13'],
-        ['MCRelVal.RelValSMS-T1tttt_mGl-1500_mLSP-100_13'],
+        ['MCRelVal-HEMissStudies.RelValTTbar_13'],
+        ['MCRelVal-HEMissStudies.RelValQCD_FlatPt_15_3000HS_13'],
+        ['MCRelVal-HEMissStudies.RelValQCD_Pt_80_120_13'],
+        ['MCRelVal-HEMissStudies.RelValQCD_Pt_600_800_13'],
+        ['MCRelVal-HEMissStudies.RelValSMS-T1tttt_mGl-1500_mLSP-100_13'],
     ]
 }
