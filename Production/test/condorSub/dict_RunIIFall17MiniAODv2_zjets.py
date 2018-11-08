b5a9a13bf9176ea03f8c951318bbd1be87eb85f7
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-11-07 18:02:49 -0600

    Add a new script to make the MC dictionaries and update the dictionaries to remove the "by-hand" scripting.

--- Production/test/condorSub/dict_RunIIFall17MiniAODv2_zjets.py
+++ Production/test/condorSub/dict_RunIIFall17MiniAODv2_zjets.py
@@ -1,20 +1,3 @@
-#Made in python using:
-'''
-import glob
-location = 'RunIIFall17MiniAODv2/'
-pattern = 'ZJetsToNuNu'
-files_grabbed = []
-ostring = ""
-files_grabbed.extend(glob.glob(location+pattern+'*'))
-
-ostring = "flist = {\n" + (4 * ' ') + "\"scenario\": \"Fall17\",\n" + (4 * ' ') + "\"samples\": [\n"
-for f in files_grabbed:
-     ostring = ostring + (8 * ' ') + "[\'"+f.replace("_cff.py","").replace("/",".")+"\'],\n"
-
-ostring = ostring + (4 * ' ') + "]\n}\n"
-print ostring
-'''
-
 flist = {
     "scenario": "Fall17",
     "samples": [
