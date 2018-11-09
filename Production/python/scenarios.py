0b289758fda60056a48f29ea96c5fbee853eba86
Kevin Pedro <kpedro88@gmail.com>
2018-11-09 11:46:08 -0600

    restore old scenarios

--- Production/python/scenarios.py
+++ Production/python/scenarios.py
@@ -1,6 +1,60 @@
 class Scenario:
     def __init__(self,sname):
-        if sname == "Summer16MiniAODv3":
+        if sname == "Spring16Fastsig":
+            self.set_vars(
+                globaltag="80X_mcRun2_asymptotic_2016_miniAODv2",
+                tagname="PAT",
+                hlttagname="HLT",
+                geninfo=True,
+                fastsim=True,
+                signal=True,
+                jecfile="data/jec/Spring16_25nsFastSimMC_V1",
+                jerfile="data/jer/Summer16_25nsV1_MC",
+                pufile="TreeMaker/Production/test/data/PileupHistograms_0721_63mb_pm5.root",
+                era="Run2_2016",
+                localera="TM2016",
+            )
+        elif sname == "Spring16Pmssm":
+            self.set_vars(
+                globaltag="80X_mcRun2_asymptotic_2016_miniAODv2",
+                tagname="PAT",
+                hlttagname="HLT",
+                geninfo=True,
+                fastsim=True,
+                signal=True,
+                pmssm=True,
+                jecfile="data/jec/Spring16_25nsFastSimMC_V1",
+                jerfile="data/jer/Summer16_25nsV1_MC",
+                pufile="TreeMaker/Production/test/data/PileupHistograms_0721_63mb_pm5.root",
+                era="Run2_2016",
+                localera="TM2016",
+            )
+        elif sname == "Summer16":
+            self.set_vars(
+                globaltag="80X_mcRun2_asymptotic_2016_TrancheIV_v6",
+                tagname="PAT",
+                hlttagname="HLT",
+                geninfo=True,
+                jecfile="data/jec/Summer16_23Sep2016V3_MC",
+                jerfile="data/jer/Summer16_25nsV1_MC",
+                pufile="TreeMaker/Production/test/data/PileupHistograms_0721_63mb_pm5.root",
+                era="Run2_2016",
+                localera="TM2016",
+            )
+        elif sname == "Summer16sig":
+            self.set_vars(
+                globaltag="80X_mcRun2_asymptotic_2016_TrancheIV_v6",
+                tagname="PAT",
+                hlttagname="HLT",
+                geninfo=True,
+                signal=True,
+                jecfile="data/jec/Summer16_23Sep2016V3_MC",
+                jerfile="data/jer/Summer16_25nsV1_MC",
+                pufile="TreeMaker/Production/test/data/PileupHistograms_0721_63mb_pm5.root",
+                era="Run2_2016",
+                localera="TM2016",
+            )
+        elif sname == "Summer16MiniAODv3":
             self.set_vars(
                 globaltag="80X_mcRun2_asymptotic_2016_TrancheIV_v6",
                 tagname="PAT",
