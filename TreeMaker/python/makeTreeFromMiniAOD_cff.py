f2bb8fd19b8b0d7a116e76ed22a5b3be62785b36
Kevin Pedro <kpedro88@gmail.com>
2018-11-15 17:10:55 -0600

    fix arg type

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -239,7 +239,7 @@ def makeTreeFromMiniAOD(self,process):
             postfix = 'UpdatedJEC',
             jetCorrections = ('AK4PFchs', levels, 'None'),
             btagDiscriminators = ak4updates.discrs.value() if len(ak4updates.discrs.value())>0 else ['None'],
-            printWarning = self.verbose,
+            printWarning = bool(self.verbose),
         )
         
         JetTag = cms.InputTag('updatedPatJetsUpdatedJEC' if len(ak4updates.discrs.value())==0 else 'updatedPatJetsTransientCorrectedUpdatedJEC')
@@ -295,7 +295,7 @@ def makeTreeFromMiniAOD(self,process):
             svSource = cms.InputTag('slimmedSecondaryVertices'),
             rParam = 0.8,
             btagDiscriminators = ak8updates if len(ak8updates)>0 else ['None'],
-            printWarning = self.verbose,
+            printWarning = bool(self.verbose),
         )
         
         # remove pt cut to avoid default values for some jets
