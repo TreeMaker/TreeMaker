6e03726e8da825458edd27b53f431405267918ab
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-11-30 10:03:57 -0600

    Update the job dictionaries and the neff dictionary.

--- Production/test/condorSub/dict_RunIIFall17MiniAODv2_singlet.py
+++ Production/test/condorSub/dict_RunIIFall17MiniAODv2_singlet.py
@@ -1,13 +1,12 @@
 flist = {
     "scenario": "Fall17",
     "samples": [
-        ['RunIIFall17MiniAODv2.ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8'],
+        ['RunIIFall17MiniAODv2.ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8'],
         ['RunIIFall17MiniAODv2.ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8'],
         ['RunIIFall17MiniAODv2.ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8'],
-        ['RunIIFall17MiniAODv2.ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8'],
-        ['RunIIFall17MiniAODv2.ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8_ext1'],
-        ['RunIIFall17MiniAODv2.ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8'],
-        ['RunIIFall17MiniAODv2.ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
-        ['RunIIFall17MiniAODv2.ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
+        ['RunIIFall17MiniAODv2.ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_PSweights_13TeV-powheg-pythia8'],
+        ['RunIIFall17MiniAODv2.ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_PSweights_13TeV-powheg-pythia8'],
+        ['RunIIFall17MiniAODv2.ST_tW_antitop_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8'],
+        ['RunIIFall17MiniAODv2.ST_tW_top_5f_inclusiveDecays_TuneCP5_PSweights_13TeV-powheg-pythia8'],
     ]
 }
--- Production/test/condorSub/dict_RunIIFall17MiniAODv2_ttbar.py
+++ Production/test/condorSub/dict_RunIIFall17MiniAODv2_ttbar.py
@@ -1,14 +1,16 @@
 flist = {
     "scenario": "Fall17",
     "samples": [
+        ['RunIIFall17MiniAODv2.TTJets_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['RunIIFall17MiniAODv2.TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['RunIIFall17MiniAODv2.TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['RunIIFall17MiniAODv2.TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8'],
+        ['RunIIFall17MiniAODv2.TTJets_SingleLeptFromT_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['RunIIFall17MiniAODv2.TTJets_SingleLeptFromTbar_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
+        ['RunIIFall17MiniAODv2.TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
+        ['RunIIFall17MiniAODv2.TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+        ['RunIIFall17MiniAODv2.TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
+        ['RunIIFall17MiniAODv2.TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
         ['RunIIFall17MiniAODv2.TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
-        ['RunIIFall17MiniAODv2.TT_TuneCUETP8M2T4_13TeV-powheg-isrdown-pythia8_ext1'],
-        ['RunIIFall17MiniAODv2.TT_TuneCUETP8M2T4_13TeV-powheg-isrdown-pythia8'],
-        ['RunIIFall17MiniAODv2.TT_TuneCUETP8M2T4_13TeV-powheg-fsrup-pythia8_ext1'],
-        ['RunIIFall17MiniAODv2.TT_TuneCUETP8M2T4_13TeV-powheg-fsrdown-pythia8_ext1'],
     ]
 }
