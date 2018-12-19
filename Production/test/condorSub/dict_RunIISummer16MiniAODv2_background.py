7da5e7d66ddcb0d1f0be23159d234cd006f27c98
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-12-19 16:14:38 -0600

    Add the condorSub dictionaries for the Summer16MiniAODv3 and Summer16MiniAODv2 datasets. Some of the MiniAODv2 datasets will need to be processed because the v3 versions aren't ready yet. Also update makeDicts.py to have better error messages. Also make sure it picks up py files only and not pyc files.

---
+++ Production/test/condorSub/dict_RunIISummer16MiniAODv2_background.py
@@ -0,0 +1,26 @@
+flist = {
+    "scenario": "Summer16",
+    "samples": [
+        ['Summer16.ZJetsToNuNu_HT-600To800_13TeV-madgraph'],
+        ['Summer16.ZJetsToNuNu_HT-800To1200_13TeV-madgraph'],
+        ['Summer16.ZJetsToNuNu_HT-1200To2500_13TeV-madgraph'],
+        ['Summer16.ZJetsToNuNu_HT-1200To2500_13TeV-madgraph_ext1'],
+        ['Summer16.ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph'],
+        ['Summer16.ST_s-channel_4f_InclusiveDecays_13TeV-amcatnlo-pythia8'],
+        ['Summer16.ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1'],
+        ['Summer16.tZq_W_lept_Z_hadron_4f_ckm_NLO_13TeV_amcatnlo_pythia8'],
+        ['Summer16.WGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph'],
+        ['Summer16.ZGTo2NuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'],
+        ['Summer16.ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8'],
+        ['Summer16.ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix_ext1'],
+        ['Summer16.ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8_ext3'],
+        ['Summer16.TTGamma_SingleLeptFromT_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8'],
+        ['Summer16.TTHH_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTTW_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTWH_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTWW_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTWZ_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTZH_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+        ['Summer16.TTZZ_TuneCUETP8M2T4_13TeV-madgraph-pythia8'],
+    ]
+}
---
+++ Production/test/condorSub/dict_RunIISummer16MiniAODv2_signal.py
@@ -0,0 +1,6 @@
+flist = {
+    "scenario": "Summer16sig",
+    "samples": [
+        ['Summer16.RPV_2t6j_mStop-500_mN1-100_TuneCUEP8M1_13TeV-madgraphMLM-pythia8'],
+    ]
+}
