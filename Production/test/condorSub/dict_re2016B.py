a7182eba5b563fdaedbc2a4a1c67ccd4ac0ce642
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-12-22 06:24:09 -0600

    Update the 2016B JetHT and 2016H MET datasets from v1 to v2. This is needed for the V16 production.

--- Production/test/condorSub/dict_re2016B.py
+++ Production/test/condorSub/dict_re2016B.py
@@ -8,7 +8,7 @@ flist = {
         ['Run2016B-17Jul2018_ver1-v1.SingleMuon'],
         ['Run2016B-17Jul2018_ver1-v1.SinglePhoton'],
         ['Run2016B-17Jul2018_ver2-v1.HTMHT'],
-        ['Run2016B-17Jul2018_ver2-v1.JetHT'],
+        ['Run2016B-17Jul2018_ver2-v2.JetHT'],
         ['Run2016B-17Jul2018_ver2-v1.MET'],
         ['Run2016B-17Jul2018_ver2-v1.SingleElectron'],
         ['Run2016B-17Jul2018_ver2-v1.SingleMuon'],
