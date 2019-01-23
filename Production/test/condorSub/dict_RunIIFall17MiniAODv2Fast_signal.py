d285e1c06aba31b5f8e65d765ee0d4fc75ad8259
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-01-23 12:04:10 -0600

    Files lists, scenarios, and dictionaries needed to produce the Summer16MiniAODv3 and Fall17MiniAODv2 fastsim ntuples.

---
+++ Production/test/condorSub/dict_RunIIFall17MiniAODv2Fast_signal.py
@@ -0,0 +1,6 @@
+flist = {
+    "scenario": "Fall17Fastsig",
+    "samples": [
+        ['RunIIFall17MiniAODv2Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8'],
+    ]
+}
---
+++ Production/test/condorSub/dict_RunIISummer16MiniAODv3Fast_signal.py
@@ -0,0 +1,7 @@
+flist = {
+    "scenario": "Summer16MiniAODv3Fastsig",
+    "samples": [
+        ['RunIISummer16MiniAODv3Fast.SMS-T1tttt_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['RunIISummer16MiniAODv3Fast.SMS-T2tt_mStop-150to250_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+    ]
+}
