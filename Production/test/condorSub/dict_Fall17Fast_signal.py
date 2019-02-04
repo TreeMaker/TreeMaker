7e8353e0ff40ce74c7de28c80b1dec72d99c36bd
Kevin Pedro <kpedro88@gmail.com>
2019-02-04 11:58:36 -0600

    update job dicts

---
+++ Production/test/condorSub/dict_Fall17Fast_signal.py
@@ -0,0 +1,23 @@
+flist = {
+    "scenario": "Fall17Fastsig",
+    "samples": [
+        ['Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Fall17Fast.SMS-T1bbbb_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T1qqqq_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Fall17Fast.SMS-T2bb_mSbot-1650to2600_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2bb_mSbot-1650to2600_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Fall17Fast.SMS-T2qq_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2qq_mSq-1850to2600_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Fall17Fast.SMS-T2tt_mStop-150to250_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Fall17Fast.SMS-T2tt_mStop-350to400_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Fall17Fast.SMS-T5qqqqVV_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'],
+    ]
+}
