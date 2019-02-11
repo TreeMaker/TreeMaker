155b1903cd0784f458c8bfc46f7dd70ab73af599
Kevin Pedro <kpedro88@gmail.com>
2019-02-11 11:47:37 -0600

    update job dicts

--- Production/test/condorSub/dict_RunIISummer16MiniAODv3_zjets.py
+++ Production/test/condorSub/dict_RunIISummer16MiniAODv3_zjets.py
@@ -7,5 +7,10 @@ flist = {
         ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-200To400_13TeV-madgraph_ext1'],
         ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-400To600_13TeV-madgraph'],
         ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-400To600_13TeV-madgraph_ext1'],
+        ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph'],
+        ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-1200To2500_13TeV-madgraph_ext1'],
+        ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-1200To2500_13TeV-madgraph'],
+        ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-800To1200_13TeV-madgraph'],
+        ['RunIISummer16MiniAODv3.ZJetsToNuNu_HT-600To800_13TeV-madgraph'],
     ]
 }
