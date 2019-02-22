0d7369404b7660dd2eb07add01f016daefe7ac4e
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-02-21 19:34:02 -0600

    Update the RunIIAutumn18MiniAOD dictionaries with the latest list of files.

--- Production/test/condorSub/dict_RunIIAutumn18MiniAOD_dyjets.py
+++ Production/test/condorSub/dict_RunIIAutumn18MiniAOD_dyjets.py
@@ -3,5 +3,11 @@ flist = {
     "samples": [
         ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8'],
         ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_ext1'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_HT-600to800_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_TuneCP1_13TeV-madgraphMLM-pythia8'],
+        ['RunIIAutumn18MiniAOD.DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8'],
     ]
 }
