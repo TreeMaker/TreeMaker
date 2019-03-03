cab0eb4052362817dc0759731637d0596e2df2c9
Kevin Pedro <kpedro88@gmail.com>
2019-03-02 19:58:06 -0600

    add updated ecalBadCalib filters

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -545,6 +545,42 @@ def makeTreeFromMiniAOD(self,process):
         self.VarsInt.extend(['ecalBadCalibFilter'])
 
         # some filters need to be rerun
+        from RecoMET.METFilters.ecalBadCalibFilter_cfi import ecalBadCalibFilter
+        process.ecalBadCalibReducedFilter = ecalBadCalibFilter.clone(
+            EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
+            ecalMinEt = cms.double(50.),
+            baddetEcal = cms.vuint32([
+                872439604,872422825,872420274,872423218,
+                872423215,872416066,872435036,872439336,
+                872420273,872436907,872420147,872439731,
+                872436657,872420397,872439732,872439339,
+                872439603,872422436,872439861,872437051,
+                872437052,872420649,872422436,872421950,
+                872437185,872422564,872421566,872421695,
+                872421955,872421567,872437184,872421951,
+                872421694,872437056,872437057,872437313
+            ]),
+            taggingMode = cms.bool(True),
+        )
+        process.ecalBadCalibReducedExtraFilter = process.ecalBadCalibReducedFilter.clone(
+            baddetEcal = cms.vuint32([
+                872439604,872422825,872420274,872423218,
+                872423215,872416066,872435036,872439336,
+                872420273,872436907,872420147,872439731,
+                872436657,872420397,872439732,872439339,
+                872439603,872422436,872439861,872437051,
+                872437052,872420649,872421950,872437185,
+                872422564,872421566,872421695,872421955,
+                872421567,872437184,872421951,872421694,
+                872437056,872437057,872437313,872438182,
+                872438951,872439990,872439864,872439609,
+                872437181,872437182,872437053,872436794,
+                872436667,872436536,872421541,872421413,
+                872421414,872421031,872423083,872421439
+            ])
+        )
+        self.VarsBool.extend(['ecalBadCalibReducedFilter','ecalBadCalibReducedExtraFilter'])
+
         process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
         process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
         process.BadChargedCandidateFilter.PFCandidates = cms.InputTag("packedPFCandidates")
