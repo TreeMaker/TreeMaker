f3e38d4b57dfe3e47684fbd3f934ddea3943f0d0
Kevin Pedro <kpedro88@gmail.com>
2019-03-04 17:09:31 -0600

    propagate renaming to job dicts

---
+++ Production/test/condorSub/dict_Autumn18_tth.py
@@ -0,0 +1,10 @@
+flist = {
+    "scenario": "Autumn18",
+    "samples": [
+        ['Autumn18.TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8_ext1'],
+        ['Autumn18.TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_ext1'],
+        ['Autumn18.TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8'],
+        ['Autumn18.TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8'],
+        ['Autumn18.TTWW_TuneCP5_13TeV-madgraph-pythia8_ext1'],
+    ]
+}
---
+++ Production/test/condorSub/dict_Summer16v3_gjets.py
@@ -0,0 +1,17 @@
+flist = {
+    "scenario": "Summer16MiniAODv3",
+    "samples": [
+        ['Summer16v3.GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Summer16v3.GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Summer16v3.GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Summer16v3.GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1'],
+        ['Summer16v3.GJets_DR-0p4_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_DR-0p4_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_DR-0p4_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+        ['Summer16v3.GJets_DR-0p4_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+    ]
+}
