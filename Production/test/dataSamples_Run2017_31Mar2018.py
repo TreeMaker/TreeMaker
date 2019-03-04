61696e5bf28009763c4b56aac18272b6b38c4321
Kevin Pedro <kpedro88@gmail.com>
2019-03-04 17:54:08 -0600

    clean up lumi dicts, add 2018 ones

---
+++ Production/test/dataSamples_Run2017_31Mar2018.py
@@ -0,0 +1,37 @@
+dataSamples = [
+    # 2017B datasets: 31Mar2018
+    [ "HTMHT_2017B",          "Run2017B-31Mar2018-v1.HTMHT_*"] ,
+    [ "JetHT_2017B",          "Run2017B-31Mar2018-v1.JetHT_*"] ,
+    [ "MET_2017B",            "Run2017B-31Mar2018-v1.MET_*"] ,
+    [ "SingleElectron_2017B", "Run2017B-31Mar2018-v1.SingleElectron_*"] ,
+    [ "SingleMuon_2017B",     "Run2017B-31Mar2018-v1.SingleMuon_*"] ,
+    [ "SinglePhoton_2017B",   "Run2017B-31Mar2018-v1.SinglePhoton_*"] ,
+    # 2017C datasets: 31Mar2018
+    [ "HTMHT_2017C",          "Run2017C-31Mar2018-v1.HTMHT_*"] ,
+    [ "JetHT_2017C",          "Run2017C-31Mar2018-v1.JetHT_*"] ,
+    [ "MET_2017C",            "Run2017C-31Mar2018-v1.MET_*"] ,
+    [ "SingleElectron_2017C", "Run2017C-31Mar2018-v1.SingleElectron_*"] ,
+    [ "SingleMuon_2017C",     "Run2017C-31Mar2018-v1.SingleMuon_*"] ,
+    [ "SinglePhoton_2017C",   "Run2017C-31Mar2018-v1.SinglePhoton_*"] ,
+    # 2017D datasets: 31Mar2018
+    [ "HTMHT_2017D",          "Run2017D-31Mar2018-v1.HTMHT_*"] ,
+    [ "JetHT_2017D",          "Run2017D-31Mar2018-v1.JetHT_*"] ,
+    [ "MET_2017D",            "Run2017D-31Mar2018-v1.MET_*"] ,
+    [ "SingleElectron_2017D", "Run2017D-31Mar2018-v1.SingleElectron_*"] ,
+    [ "SingleMuon_2017D",     "Run2017D-31Mar2018-v1.SingleMuon_*"] ,
+    [ "SinglePhoton_2017D",   "Run2017D-31Mar2018-v1.SinglePhoton_*"] ,
+    # 2017E datasets: 31Mar2018
+    [ "HTMHT_2017E",          "Run2017E-31Mar2018-v1.HTMHT_*"] ,
+    [ "JetHT_2017E",          "Run2017E-31Mar2018-v1.JetHT_*"] ,
+    [ "MET_2017E",            "Run2017E-31Mar2018-v1.MET_*"] ,
+    [ "SingleElectron_2017E", "Run2017E-31Mar2018-v1.SingleElectron_*"] ,
+    [ "SingleMuon_2017E",     "Run2017E-31Mar2018-v1.SingleMuon_*"] ,
+    [ "SinglePhoton_2017E",   "Run2017E-31Mar2018-v1.SinglePhoton_*"] ,
+    # 2017F datasets: 31Mar2018
+    [ "HTMHT_2017F",          "Run2017F-31Mar2018-v1.HTMHT_*"] ,
+    [ "JetHT_2017F",          "Run2017F-31Mar2018-v1.JetHT_*"] ,
+    [ "MET_2017F",            "Run2017F-31Mar2018-v1.MET_*"] ,
+    [ "SingleElectron_2017F", "Run2017F-31Mar2018-v1.SingleElectron_*"] ,
+    [ "SingleMuon_2017F",     "Run2017F-31Mar2018-v1.SingleMuon_*"] ,
+    [ "SinglePhoton_2017F",   "Run2017F-31Mar2018-v1.SinglePhoton_*"] ,
+]
