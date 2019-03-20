3974ad481eb8c8c1b085b29f57f6d2ac4c4dec98
Kevin Pedro <kpedro88@gmail.com>
2019-03-20 14:05:23 -0500

    split 2018D dict for submission

--- Production/test/condorSub/dict_2018D_part1.py
+++ Production/test/condorSub/dict_2018D_part1.py
@@ -2,8 +2,6 @@ flist = {
     "scenario": "2018PromptReco",
     "samples": [
         ['Run2018D-22Jan2019-v2.EGamma'],
-        ['Run2018D-PromptReco-v2.JetHT'],
         ['Run2018D-PromptReco-v2.MET'],
-        ['Run2018D-22Jan2019-v2.SingleMuon'],
     ]
 }
