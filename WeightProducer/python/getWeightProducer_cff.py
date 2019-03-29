6101bff242b96ef30a1485b94d2e626e6212dc34
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-03-29 09:59:11 -0500

    Update the year-by-year XS and k-factor values. Values taken from XSDB, https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns, and GenXSecAnalyzer.

--- WeightProducer/python/getWeightProducer_cff.py
+++ WeightProducer/python/getWeightProducer_cff.py
@@ -812,7 +812,7 @@ def getWeightProducer(fileName,fastsim=False, pmssm=False):
     for sample in samples:
         if sample.name in fileName and sample.production in fileName:
             weightProducer.Method     = cms.string(sample.Method)
-            weightProducer.XS         = cms.double(sample.XS)
+            weightProducer.XS         = cms.double(sample.XS*sample.kFactor)
             weightProducer.NumberEvts = cms.double(sample.NumberEvtsDiff)
             weightProducer.RemakePU   = cms.bool(sample.WrongPU)
             print sample.name+", "+sample.production+" : '"+fileName+"'"
