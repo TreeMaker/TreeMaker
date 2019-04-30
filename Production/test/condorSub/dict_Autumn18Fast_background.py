9e54d04ef45e633e50d238538214bb719c752f7f
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-04-30 08:47:38 -0500

    Add in the Autumn18 FastSim condor dictionaries.

---
+++ Production/test/condorSub/dict_Autumn18Fast_background.py
@@ -0,0 +1,8 @@
+flist = {
+    "scenario": "Autumn18Fast",
+    "samples": [
+        ['Autumn18Fast.TTJets_SingleLeptFromT_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Autumn18Fast.TTJets_SingleLeptFromTbar_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Autumn18Fast.TTJets_DiLept_TuneCP2_13TeV-madgraphMLM-pythia8'],
+    ]
+}
