1de69b23cc2b3c743f9ffb452c32de8159c42e67
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2019-05-06 13:49:53 -0500

    Add some additional Autumn18 fastsim signal samples.

--- Production/test/condorSub/dict_Autumn18Fast_signal.py
+++ Production/test/condorSub/dict_Autumn18Fast_signal.py
@@ -3,7 +3,10 @@ flist = {
     "samples": [
         ['Autumn18Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8'],
         ['Autumn18Fast.SMS-T1qqqq_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Autumn18Fast.SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8'],
         ['Autumn18Fast.SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8'],
         ['Autumn18Fast.SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Autumn18Fast.SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8'],
+        ['Autumn18Fast.SMS-T2qq_mSq-1850to2600_TuneCP2_13TeV-madgraphMLM-pythia8'],
     ]
 }
