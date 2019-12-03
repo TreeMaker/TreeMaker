239308f6583bf5dfd77bc9896b4ffb5e4a674b7e
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-12-02 18:19:11 -0600

    Updated the ttbar dict for the new samples

--- Production/test/condorSub/dict_Fall17_ttbar.py
+++ Production/test/condorSub/dict_Fall17_ttbar.py
@@ -16,5 +16,21 @@ flist = {
         ['Fall17.TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8'],
         ['Fall17.TTToSemiLeptonic_TuneCP2_PSweights_13TeV-powheg-pythia8'],
         ['Fall17.TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8'],
+
+        ['Fall17.TTTo2L2Nu_TuneCP5_erdON_13TeV-powheg-pythia8'],
+        ['Fall17.TTTo2L2Nu_TuneCP5down_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTTo2L2Nu_TuneCP5up_PSweights_13TeV-powheg-pythia8'],
+        ['Fall17.TTTo2L2Nu_hdampDOWN_TuneCP5_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTTo2L2Nu_hdampUP_TuneCP5_PSweights_13TeV-powheg-pythia8'],
+        ['Fall17.TTToHadronic_TuneCP5_erdON_13TeV-powheg-pythia8'],
+        ['Fall17.TTToHadronic_TuneCP5down_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToHadronic_TuneCP5up_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToHadronic_hdampDOWN_TuneCP5_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToHadronic_hdampUP_TuneCP5_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToSemiLeptonic_TuneCP5_erdON_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToSemiLeptonic_TuneCP5down_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToSemiLeptonic_TuneCP5up_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToSemiLeptonic_hdampDOWN_TuneCP5_PSweights_13TeV-powheg-pythia8'], 
+        ['Fall17.TTToSemiLeptonic_hdampUP_TuneCP5_PSweights_13TeV-powheg-pythia8'], 
     ]
 }
