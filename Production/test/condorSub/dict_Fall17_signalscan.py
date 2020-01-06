e6846ae2270a7a8b113ebd45fc0263da90819ae0
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-01-06 17:21:41 -0600

    Add the corresponding condorSub dictionaries.

---
+++ Production/test/condorSub/dict_Fall17_signalscan.py
@@ -0,0 +1,6 @@
+flist = {
+    "scenario": "Fall17sigscan",
+    "samples": [
+        ['Fall17.SMS-T5qqqqZH-mGluino-1000to2500_TuneCP2_13TeV-madgraphMLM-pythia8'],
+    ]
+}
---
+++ Production/test/condorSub/dict_Summer16v3_signalscan.py
@@ -0,0 +1,6 @@
+flist = {
+    "scenario": "Summer16v3sigscan",
+    "samples": [
+        ['Summer16v3.SMS-T5qqqqZH-mGluino-1000to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'],
+    ]
+}
