f99bd29de607d498ce0ce0d5e8d4d3cdfab1ce4e
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-04-24 23:56:44 -0500

    Remove all references to the unneeded IsolationProducer.

--- TreeMaker/python/doLostLeptonBkg.py
+++ TreeMaker/python/doLostLeptonBkg.py
@@ -8,27 +8,6 @@ def doLostLeptonBkg(self,process,METTag):
             pfCandsTag  = cms.InputTag('packedPFCandidates')
         )
 
-#    from TreeMaker.TreeMaker.TMEras import TMeras
-#    from TreeMaker.Utils.isolationproducer_cfi import isolationproducer
-#    process.IDMuonMiniIso = isolationproducer.clone(
-#        LeptonTag = cms.InputTag('LeptonsNew:IdMuon'),
-#        LeptonType = cms.string('muon')
-#    )
-#    electronEAValuesLocal = cms.vdouble(0.1566, 0.1626, 0.1073, 0.0854, 0.1051, 0.1204, 0.1524)
-#    muonEAValuesLocal     = cms.vdouble(0.0735, 0.0619, 0.0465, 0.0433, 0.0577)
-#    (TMeras.TM2017 | TMeras.TM2018).toModify(process.IDMuonMiniIso,
-#        electronEAValues = electronEAValuesLocal,
-#        muonEAValues     = muonEAValuesLocal
-#    )
-#    process.IDElectronMiniIso = isolationproducer.clone(
-#        LeptonTag = cms.InputTag('LeptonsNew:IdElectron'), 
-#        LeptonType = cms.string('electron')
-#    )
-#    (TMeras.TM2017 | TMeras.TM2018).toModify(process.IDElectronMiniIso,
-#        electronEAValues = electronEAValuesLocal,
-#        muonEAValues     = muonEAValuesLocal
-#    )
-
     from TreeMaker.Utils.trackIsolationMaker_cfi import trackIsolationFilter
 
     process.TAPElectronTracks = trackIsolationFilter.clone(
@@ -69,39 +48,12 @@ def doLostLeptonBkg(self,process,METTag):
             mTCut               = 0.,
             METTag              = METTag
             )
-
-#    if self.geninfo:
-#        process.GenMuonMiniIso = isolationproducer.clone(
-#            LeptonTag = cms.InputTag('GenLeptons:Muon'), 
-#            LeptonType = cms.string('gen')
-#        )
-#        (TMeras.TM2017 | TMeras.TM2018).toModify(process.GenMuonMiniIso,
-#            electronEAValues = electronEAValuesLocal,
-#            muonEAValues     = muonEAValuesLocal
-#        )
-#        process.GenElectronMiniIso = isolationproducer.clone(
-#            LeptonTag = cms.InputTag('GenLeptons:Electron'), 
-#            LeptonType = cms.string('gen')
-#        )
-#        (TMeras.TM2017 | TMeras.TM2018).toModify(process.GenElectronMiniIso,
-#            electronEAValues = electronEAValuesLocal,
-#            muonEAValues     = muonEAValuesLocal
-#        )
-#        process.GenTauMiniIso = isolationproducer.clone(
-#            LeptonTag = cms.InputTag('GenLeptons:Tau'), 
-#            LeptonType = cms.string('gen')
-#        )
-#        (TMeras.TM2017 | TMeras.TM2018).toModify(process.GenTauMiniIso,
-#            electronEAValues = electronEAValuesLocal,
-#            muonEAValues     = muonEAValuesLocal
-#        )
     
     for type in ['Electron','Muon']:
         self.VectorBool.extend(['LeptonsNew:Id'+type+'MediumID('+type+'s_mediumID)'])
         self.VectorBool.extend(['LeptonsNew:Id'+type+'TightID('+type+'s_tightID)'])
         self.VectorDouble.extend(['LeptonsNew:Id'+type+'MTW('+type+'s_MTW)'])
         self.VectorDouble.extend(['LeptonsNew:Id'+type+'Iso('+type+'s_iso)'])
-#        self.VectorDouble.extend(['ID'+type+'MiniIso:MiniIso('+type+'s_MiniIso)'])
 
     for type in ['Electron','Muon','Pion']:
         self.VectorTLorentzVector.extend(['TAP'+type+'Tracks:pfcands(TAP'+type+'Tracks)'])
