a0f5334458ab7e1cee15ec86175596305c3d2f25
Enoch Chen <enochnotsocool@gmail.com>
2020-06-12 22:16:59 -0400

    Added all EMJ samples cff file

---
+++ Production/python/PrivateSamples/EMJ_2017_mMed-1500_mDark-20_ctau-60_unflavored-down_cff.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend( [
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-1.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-2.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-3.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-4.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-5.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-6.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-7.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-8.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-9.root',
+       '/mnt/hadoop/cms/store/group/EMJRunII/2017/step4_MINIAOD_mMed-1500_mDark-20_ctau-60_unflavored-down_n-500_part-10.root',
+] )
