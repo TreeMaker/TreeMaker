43a99c00d3b4f06838e2ad4cdcd7d7c464ce2458
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-07-29 22:54:42 -0500

    Implement the proxy exporter needed by ServiceX.

--- .github/ServiceX/scripts/proxy-exporter.sh
+++ .github/ServiceX/scripts/proxy-exporter.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-#sudo mkdir -p /etc/grid-security
-#sudo chown cmsuser /etc/grid-security
+sudo mkdir -p /etc/grid-security
+sudo chown cmsusr /etc/grid-security
 
 
 while true; do
-    #sudo cp /etc/grid-security-ro/x509up /etc/grid-security
-    #sudo chown cmsuser /etc/grid-security/x509up
-    #chmod 600 /etc/grid-security/x509up
+    sudo cp /etc/grid-security-ro/x509up /etc/grid-security
+    sudo chown cmsusr /etc/grid-security/x509up
+    chmod 600 /etc/grid-security/x509up
 
     # Refresh every hour
     sleep 3600
