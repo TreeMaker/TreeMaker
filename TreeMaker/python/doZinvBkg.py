8ac23bbdf9ee163894246b27e2889b2633955b81
Kevin Pedro <kpedro88@gmail.com>
2020-09-08 16:31:26 -0500

    remove unused met orig

--- TreeMaker/python/doZinvBkg.py
+++ TreeMaker/python/doZinvBkg.py
@@ -255,14 +255,6 @@ def reclusterZinv(self, process, cleanedCandidates, suff):
     self.VarsDouble.extend(['METclean'+suff+':Pt(METclean'+suff+')','METclean'+suff+':Phi(METPhiclean'+suff+')'])
 #    self.VarsDouble.extend(['METclean'+suff+':RawPt(RawMETclean'+suff+')','METclean'+suff+':RawPhi(RawMETPhiclean'+suff+')'])
 
-    if self.doMETfix:
-        METcleanOrig = METclean.clone(
-            METTag = METTagOrig
-        )
-        setattr(process,"METclean"+suff+"Orig",METcleanOrig)
-        self.VarsDouble.extend(['METclean'+suff+'Orig:Pt(METclean'+suff+'Orig)','METclean'+suff+'Orig:Phi(METPhiclean'+suff+'Orig)'])
-#        self.VarsDouble.extend(['METclean'+suff+'Orig:RawPt(RawMETclean'+suff+'Orig)','METclean'+suff+'Orig:RawPhi(RawMETPhiclean'+suff+'Orig)'])
-
     return process
 
 def doZinvBkg(self,process):
