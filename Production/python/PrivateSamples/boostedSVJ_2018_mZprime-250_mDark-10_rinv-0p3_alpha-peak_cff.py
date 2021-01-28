b032b184a1ac684efc2be8bb10a9cfe67db28b22
Thomas Klijnsma <klijnsma@fnal.gov>
2021-01-28 11:11:20 -0600

    fixes to unittest

--- Production/python/PrivateSamples/boostedSVJ_2018_mZprime-250_mDark-10_rinv-0p3_alpha-peak_cff.py
+++ Production/python/PrivateSamples/boostedSVJ_2018_mZprime-250_mDark-10_rinv-0p3_alpha-peak_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
