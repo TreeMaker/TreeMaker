b8d054ab406c0cef4106a43bd75e726dc5d3fee0
Thomas Klijnsma <klijnsma@fnal.gov>
2021-01-28 10:27:14 -0600

    added unittest

---
+++ Production/python/PrivateSamples/boostedSVJ_2018_mZprime-250_mDark-10_rinv-0p3_alpha-peak_cff.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend([
+    '/store/user/lpcsusyhad/SVJ2017/boosted/miniaod/jetpt250_Sep29_year2018_mz250_epf30000_series7000/7000.root',
+])
