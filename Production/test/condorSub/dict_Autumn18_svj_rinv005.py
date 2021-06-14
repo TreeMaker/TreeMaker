a81dd5d6925b88c33020b82e194c79bec864dbc4
Kevin Pedro <kpedro88@gmail.com>
2021-06-14 09:45:37 -0500

    add file lists and dicts for rinv 0.05

---
+++ Production/test/condorSub/dict_Autumn18_svj_rinv005.py
@@ -0,0 +1,7 @@
+flist = {
+    "scenario": "Autumn18sig",
+    "samples": [
+        ['PrivateSamples.MC2018_SVJ_ScanRinv005_2018_TuneCP2_13TeV_pythia8_n-1000'],
+    ]
+}
+
---
+++ Production/test/condorSub/dict_Summer16v3_svj_rinv005.py
@@ -0,0 +1,7 @@
+flist = {
+    "scenario": "Summer16v3sig",
+    "samples": [
+        ['PrivateSamples.MC2016_SVJ_ScanRinv005_2016_TuneCUETP8M1_13TeV_pythia8_n-1000'],
+    ]
+}
+
