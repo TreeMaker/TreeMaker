a060a151f9c823f3c9ecaaf0255be6f3edf2dc4e
Semra <Semra.Turkcapar@cern.ch>
2021-07-15 12:49:43 -0500

    fixed the other empty dictionary for Summer20UL16APV

--- Production/test/condorSub/ul_condorSubmit_dictionaries/dict_Summer20UL16APV_tth.py
+++ Production/test/condorSub/ul_condorSubmit_dictionaries/dict_Summer20UL16APV_tth.py
@@ -3,6 +3,12 @@ flist = {
    "scenario": "Summer20UL16APV",
    "samples": [
 
+       ['Summer20UL16APV.TTGamma_Dilept_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL16APV.TTGamma_Hadronic_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL16APV.TTGamma_SingleLept_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL16APV.TTWW_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL16APV.TTWZ_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL16APV.TTZZ_TuneCP5_13TeV-madgraph-pythia8'],
 
    ]
 }
