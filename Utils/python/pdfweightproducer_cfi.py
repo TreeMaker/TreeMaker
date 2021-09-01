0fc6d464782d7e68540d954ccbef57517725723a
Kevin Pedro <kpedro88@gmail.com>
2021-08-31 19:48:19 -0500

    add r/f scale uncertainty recalculation for pythia-only

--- Utils/python/pdfweightproducer_cfi.py
+++ Utils/python/pdfweightproducer_cfi.py
@@ -4,7 +4,10 @@ PDFWeightProducer = cms.EDProducer("PDFWeightProducer",
     nScales = cms.uint32(9),
     nPDFs = cms.uint32(100),
     nPSs = cms.uint32(14),
+    nQCD = cms.uint32(0),
+    nEM = cms.uint32(0),
     normalize = cms.bool(True),
+    pythiaSettings = cms.vstring(),
 )
 
 from TreeMaker.TreeMaker.TMEras import TMeras
