007f2b8dba23073bc6dfa63c18a6f68063f599c7
Josh Hiltbrand <hiltbran@umn.edu>
2021-09-01 07:28:25 -0500

    no special recipe in UL to rerun certain ECAL MET filters, added muon dZ

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -536,43 +536,6 @@ def makeTreeFromMiniAOD(self,process):
         )
         self.VarsInt.extend(['ecalBadCalibFilter'])
 
-        # some filters need to be rerun
-        from RecoMET.METFilters.ecalBadCalibFilter_cfi import ecalBadCalibFilter
-        process.ecalBadCalibReducedFilter = ecalBadCalibFilter.clone(
-            EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
-            ecalMinEt = cms.double(50.),
-            baddetEcal = cms.vuint32([
-                872439604,872422825,872420274,872423218,
-                872423215,872416066,872435036,872439336,
-                872420273,872436907,872420147,872439731,
-                872436657,872420397,872439732,872439339,
-                872439603,872422436,872439861,872437051,
-                872437052,872420649,872422436,872421950,
-                872437185,872422564,872421566,872421695,
-                872421955,872421567,872437184,872421951,
-                872421694,872437056,872437057,872437313
-            ]),
-            taggingMode = cms.bool(True),
-        )
-        process.ecalBadCalibReducedExtraFilter = process.ecalBadCalibReducedFilter.clone(
-            baddetEcal = cms.vuint32([
-                872439604,872422825,872420274,872423218,
-                872423215,872416066,872435036,872439336,
-                872420273,872436907,872420147,872439731,
-                872436657,872420397,872439732,872439339,
-                872439603,872422436,872439861,872437051,
-                872437052,872420649,872421950,872437185,
-                872422564,872421566,872421695,872421955,
-                872421567,872437184,872421951,872421694,
-                872437056,872437057,872437313,872438182,
-                872438951,872439990,872439864,872439609,
-                872437181,872437182,872437053,872436794,
-                872436667,872436536,872421541,872421413,
-                872421414,872421031,872423083,872421439
-            ])
-        )
-        self.VarsBool.extend(['ecalBadCalibReducedFilter','ecalBadCalibReducedExtraFilter'])
-
         process.load('RecoMET.METFilters.BadChargedCandidateFilter_cfi')
         process.BadChargedCandidateFilter.vtx = cms.InputTag("offlineSlimmedPrimaryVertices")
         process.BadChargedCandidateFilter.muons = cms.InputTag("slimmedMuons")
@@ -587,6 +550,15 @@ def makeTreeFromMiniAOD(self,process):
         process.BadPFMuonFilter.taggingMode = True
         self.VarsBool.extend(['BadPFMuonFilter'])
         
+        # https://twiki.cern.ch/twiki/bin/view/CMS/MissingETOptionalFiltersRun2#Recipe_for_BadPFMuonDz_filter_in
+        process.load('RecoMET.METFilters.BadPFMuonDzFilter_cfi')
+        process.BadPFMuonDzFilter.vtx = cms.InputTag("offlineSlimmedPrimaryVertices")
+        process.BadPFMuonDzFilter.muons = cms.InputTag("slimmedMuons")
+        process.BadPFMuonDzFilter.PFCandidates = cms.InputTag("packedPFCandidates")
+        process.BadPFMuonDzFilter.taggingMode = True
+        process.BadPFMuonDzFilter.minDzBestTrack = cms.double(0.5)
+        self.VarsBool.extend(['BadPFMuonDzFilter'])
+
     ## ----------------------------------------------------------------------------------------------
     ## Triggers
     ## ----------------------------------------------------------------------------------------------
