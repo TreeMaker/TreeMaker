adeffcc9f536a4d7481a12f5b95458d346ba1890
Josh Hiltbrand <hiltbran@umn.edu>
2021-09-01 06:37:07 -0500

    remove reference to old MC samples

--- WeightProducer/python/getWeightProducer_cff.py
+++ WeightProducer/python/getWeightProducer_cff.py
@@ -42,19 +42,11 @@ def getWeightProducer(fileName, scan=False):
     
     # list of samples
     samples = []
-    from MCSamples_Summer16 import Summer16samples
-    from MCSamples_Fall17 import Fall17samples
-    from MCSamples_Summer16v3 import Summer16v3samples
-    from MCSamples_Autumn18 import Autumn18samples
     from MCSamples_Summer20UL16 import Summer20UL16samples
     from MCSamples_Summer20UL16APV import Summer20UL16APVsamples
     from MCSamples_Summer20UL17 import Summer20UL17samples
     from MCSamples_Summer20UL18 import Summer20UL18samples
     from SVJsamples import SVJsamples
-    samples += Summer16samples
-    samples += Fall17samples
-    samples += Summer16v3samples
-    samples += Autumn18samples
     samples += Summer20UL16samples
     samples += Summer20UL16APVsamples
     samples += Summer20UL17samples
