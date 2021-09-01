5e1df980c9737b54db8fd8f0c58d11c8f0ea0678
Josh Hiltbrand <hiltbran@umn.edu>
2021-09-01 06:40:35 -0500

    simplify now that TM80 is gone

--- Utils/python/prescaleweightproducer_cfi.py
+++ Utils/python/prescaleweightproducer_cfi.py
@@ -5,9 +5,5 @@ prescaleweightProducer = cms.EDProducer('PrescaleWeightProducer',
    prescales = cms.InputTag("patTrigger"),
    prescalesL1min = cms.InputTag("patTrigger:l1min"),
    prescalesL1max = cms.InputTag("patTrigger:l1max"),   
-   objects = cms.InputTag("selectedPatTrigger"),
+   objects = cms.InputTag("slimmedPatTrigger"),
 )
-
-from TreeMaker.TreeMaker.TMEras import TMeras
-(TMeras.TM2016 & ~TMeras.TM80X).toModify(prescaleweightProducer, objects = cms.InputTag("slimmedPatTrigger"))
-(TMeras.TM2017 | TMeras.TM2018).toModify(prescaleweightProducer, objects = cms.InputTag("slimmedPatTrigger"))
