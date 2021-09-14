d01ea7dfa8c9a61a0aa8bd10ebf21d2e5251b2a1
Josh Hiltbrand <hiltbran@umn.edu>
2021-09-14 09:13:06 -0500

    all years same for pdfs

--- Utils/python/pdfweightproducer_cfi.py
+++ Utils/python/pdfweightproducer_cfi.py
@@ -2,12 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 PDFWeightProducer = cms.EDProducer("PDFWeightProducer",
     nScales = cms.uint32(9),
-    nPDFs = cms.uint32(100),
+    nPDFs = cms.uint32(102),
     nPSs = cms.uint32(14),
     normalize = cms.bool(True),
-)
-
-from TreeMaker.TreeMaker.TMEras import TMeras
-(TMeras.TMUL2017 | TMeras.TMUL2018).toModify(PDFWeightProducer,
-    nPDFs = 102,
 )
