c2022239d98868b8c39f284e9ea4f3ab0cdb5e77
Josh Hiltbrand <hiltbran@umn.edu>
2021-09-23 14:16:49 -0500

    consolidate tth into tt

--- Production/test/condorSub/dict_Summer20UL17_tt.py
+++ Production/test/condorSub/dict_Summer20UL17_tt.py
@@ -1,6 +1,22 @@
 flist = {
-    "scenario": "Summer20UL17",
-    "samples": [
-        ['Summer20UL17.TTToHadronic_TuneCP5_13TeV-powheg-pythia8'],
-    ]
+
+   "scenario": "Summer20UL17",
+   "samples": [
+
+       ['Summer20UL17.TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8'],
+       ['Summer20UL17.TTZToNuNu_TuneCP5_13TeV-amcatnlo-pythia8'],
+       ['Summer20UL17.TTZToQQ_Dilept_TuneCP5_13TeV-amcatnlo-pythia8'],
+       ['Summer20UL17.TTZToQQ_TuneCP5_13TeV-amcatnlo-pythia8'],
+       ['Summer20UL17.TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8'],
+       ['Summer20UL17.TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8'],
+       ['Summer20UL17.TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8'],
+       ['Summer20UL17.TTGamma_Dilept_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.TTGamma_Hadronic_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.TTGamma_SingleLept_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.TTWW_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.TTWZ_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.TTZZ_TuneCP5_13TeV-madgraph-pythia8'],
+       ['Summer20UL17.ttHJetTobb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8'],
+
+   ]
 }
