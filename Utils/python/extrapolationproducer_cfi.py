498b7c1bad1c3b98881157b749e5e47e3a5ba87a
Kevin Pedro <kpedro88@gmail.com>
2021-11-01 15:02:39 -0500

    delete unused classes

--- Utils/python/extrapolationproducer_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-extrapolationproducer = cms.EDProducer('ExtrapolationProducer',
-                                       MuonTag = cms.InputTag('LeptonsNew:IdIsoMuon'),
-                                       ElectronTag = cms.InputTag('LeptonsNew:IdIsoElectron')
-                                       )
