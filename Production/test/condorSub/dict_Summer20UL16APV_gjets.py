f28a8ad674a20037f8ef324fdf10cea93ff243fc
Josh Hiltbrand <hiltbran@umn.edu>
2022-01-17 07:30:24 -0600

    add in some GJets samples

--- Production/test/condorSub/dict_Summer20UL16APV_gjets.py
+++ Production/test/condorSub/dict_Summer20UL16APV_gjets.py
@@ -3,10 +3,15 @@ flist = {
    "scenario": "Summer20UL16APV",
    "samples": [
 
+       ['Summer20UL16APV.GJets_DR-0p4_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.GJets_DR-0p4_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.GJets_DR-0p4_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.GJets_DR-0p4_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16APV.GJets_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16APV.GJets_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16APV.GJets_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16APV.GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.GJets_HT-600ToInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
 
    ]
 }
