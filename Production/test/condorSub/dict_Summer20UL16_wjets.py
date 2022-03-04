3802fb2c0fc630c88344f9aaf1cfab3199b46a9d
Josh Hiltbrand <hiltbran@umn.edu>
2022-03-04 13:38:18 -0600

    add WJetsToQQ

--- Production/test/condorSub/dict_Summer20UL16_wjets.py
+++ Production/test/condorSub/dict_Summer20UL16_wjets.py
@@ -13,6 +13,10 @@ flist = {
        ['Summer20UL16.WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16.WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL16.WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16.WJetsToQQ_HT-200to400_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16.WJetsToQQ_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16.WJetsToQQ_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16.WJetsToQQ_HT-800toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
 
    ]
 }
--- Production/test/condorSub/dict_Summer20UL17_wjets.py
+++ Production/test/condorSub/dict_Summer20UL17_wjets.py
@@ -13,10 +13,11 @@ flist = {
        ['Summer20UL17.WJetsToLNu_HT-70To100_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL17.WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
        ['Summer20UL17.WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8'],
-       ['Summer20UL17.WJetsToQQ_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8'],
-       ['Summer20UL17.WJetsToQQ_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8'],
-       ['Summer20UL17.WJetsToQQ_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8'],
-       ['Summer20UL17.WJetsToQQ_HT-800ToInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL17.WJetsToQQ_HT-200to400_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL17.WJetsToQQ_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL17.WJetsToQQ_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL17.WJetsToQQ_HT-800toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
+
 
    ]
 }
