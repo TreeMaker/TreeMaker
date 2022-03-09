2ce064957ad3b15a47439b9d848c81dbb6c872d1
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-03-09 11:37:22 +0100

    Added first set of sample configurations

---
+++ Production/python/PrivateSamples/EMJ_UL18_mMed-1200_mDark-20_ctau-25_unflavored-down_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+  readFiles.extend( [
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-1.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-10.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-2.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-3.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-4.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-5.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-6.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-7.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-8.root',  
+'root://hepcms-0.umd.edu:1094////store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-1200_mDark-20_ctau-25_unflavored-down_n-500_part-9.root',
+] )
