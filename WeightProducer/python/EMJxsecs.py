f4e45b77311dac477c6ba28b4c22f864c36cc339
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-03-11 14:49:31 +0100

    Fixed merge conflict

--- WeightProducer/python/EMJxsecs.py
+++ WeightProducer/python/EMJxsecs.py
@@ -7,4 +7,9 @@ EMJxsecs = {
 "mMed-1400": {"CrossSection":XSValues(XS_13TeV=0.001419, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") },
 "mMed-1500": {"CrossSection":XSValues(XS_13TeV=0.000771, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") },
 "mMed-1600": {"CrossSection":XSValues(XS_13TeV=0.000426, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
+"mMed-1800": {"CrossSection":XSValues(XS_13TeV=0.0001353, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
+"mMed-2000": {"CrossSection":XSValues(XS_13TeV=4.44e-05, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
+"mMed-2200": {"CrossSection":XSValues(XS_13TeV=1.50e-05, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
+"mMed-2400": {"CrossSection":XSValues(XS_13TeV=5.13e-06, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
+"mMed-2500": {"CrossSection":XSValues(XS_13TeV=3.00e-06, XSSource_13TeV="SUSY XS - https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom (x3 for color)") }
 }
