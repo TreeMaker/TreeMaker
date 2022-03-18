ae28c5e4dd9dac4220457b7c1618b0682b16bf39
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-03-18 17:34:21 +0100

    Added missing UL18 files

--- Production/python/PrivateSamples/EMJ_UL18_mMed-2400_mDark-20_ctau-5_unflavored-down_cff.py
+++ Production/python/PrivateSamples/EMJ_UL18_mMed-2400_mDark-20_ctau-5_unflavored-down_cff.py
@@ -6,6 +6,7 @@ source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = se
 readFiles.extend( [
 'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-1.root',
 'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-10.root',
+'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-11.root',
 'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-2.root',
 'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-3.root',
 'root://hepcms-0.umd.edu:1094///store/group/EMJRunII/Oct2021/UL18/step4_MINIAODv2_mMed-2400_mDark-20_ctau-5_unflavored-down_n-500_part-5.root',
