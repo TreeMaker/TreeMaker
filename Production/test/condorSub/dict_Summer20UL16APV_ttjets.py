92f372532191551e332f70aaffb85a8da57d115c
Josh Hiltbrand <hiltbran@umn.edu>
2022-03-31 14:25:08 -0500

    update dictionary

--- Production/test/condorSub/dict_Summer20UL16APV_ttjets.py
+++ Production/test/condorSub/dict_Summer20UL16APV_ttjets.py
@@ -4,6 +4,11 @@ flist = {
    "args": "boostedsemivisible=True",
    "samples": [
 
+       ['Summer20UL16APV.TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL16APV.TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
 
    ]
 }
--- Production/test/condorSub/dict_Summer20UL18_ttjets.py
+++ Production/test/condorSub/dict_Summer20UL18_ttjets.py
@@ -4,6 +4,12 @@ flist = {
    "args": "boostedsemivisible=True",
    "samples": [
 
+       ['Summer20UL18.TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL18.TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL18.TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL18.TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL18.TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8'],
+       ['Summer20UL18.TTJets_SingleLeptFromT_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8'],
 
    ]
 }
