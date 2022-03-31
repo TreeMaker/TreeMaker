5ac0c748c1a51d0041b048b50d6afa997b225941
Josh Hiltbrand <hiltbran@umn.edu>
2022-03-31 10:00:47 -0500

    update samples for singlet collection

--- Production/test/condorSub/dict_Summer20UL17_singlet.py
+++ Production/test/condorSub/dict_Summer20UL17_singlet.py
@@ -4,8 +4,10 @@ flist = {
    "samples": [
 
        ['Summer20UL17.ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8'],
-       ['Summer20UL17.ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8'],
-       ['Summer20UL17.ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8'],
+       ['Summer20UL17.ST_t-channel_antitop_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
+       ['Summer20UL17.ST_t-channel_top_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
+       ['Summer20UL17.ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
+       ['Summer20UL17.ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8'],
        ['Summer20UL17.tZq_ll_4f_ckm_NLO_TuneCP5_13TeV-amcatnlo-pythia8'],
 
    ]
