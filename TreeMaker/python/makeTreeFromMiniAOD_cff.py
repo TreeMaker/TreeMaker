12102f9e724b51944358c977d2aed1c0506c0a4d
Kevin Pedro <kpedro88@gmail.com>
2022-07-06 09:25:49 -0500

    avoid genjets in data

--- TreeMaker/python/makeTreeFromMiniAOD_cff.py
+++ TreeMaker/python/makeTreeFromMiniAOD_cff.py
@@ -386,7 +386,7 @@ def makeTreeFromMiniAOD(self,process):
             )
 
             # todo: revamp jet toolbox to handle this
-            if not self.doZinv: # calling jet toolbox again for ak8 would break after this, so skip it
+            if self.geninfo and not self.doZinv: # calling jet toolbox again for ak8 would break after this, so skip it
                 process = self.transformJetSeq(process, "ak8GenJetsNoNu", {"SoftDrop":"ak8GenJetsNoNuSoftDrop"}, "recoGenJets")
             process.ak8PFJetsPuppiLowCutSoftDrop.jetPtMin = cms.double(10) # to match central clustering
             process = self.transformJetSeq(process, "ak8PFJetsPuppiLowCut", {"SoftDrop":"ak8PFJetsPuppiLowCutSoftDrop"}, "recoPFJets")
@@ -1281,7 +1281,8 @@ def makeTreeFromMiniAOD(self,process):
             matched = cms.InputTag("ak15PFJetsPuppiSoftDropBeta1")
         )
 
-        process = self.transformJetSeq(process, "ak15GenJetsNoNu", {"SoftDrop":"ak15GenJetsNoNuSoftDrop"}, "recoGenJets")
+        if self.geninfo:
+            process = self.transformJetSeq(process, "ak15GenJetsNoNu", {"SoftDrop":"ak15GenJetsNoNuSoftDrop"}, "recoGenJets")
         process = self.transformJetSeq(process, "ak15PFJetsPuppi", {"SoftDrop":"ak15PFJetsPuppiSoftDrop", "SoftDropBeta1":"ak15PFJetsPuppiSoftDropBeta1"}, "recoPFJets")
 
         # update userfloats (used for jet ID, including ID for JEC/JER variations)
@@ -1344,7 +1345,8 @@ def makeTreeFromMiniAOD(self,process):
         self.JetsNames.append("JetsAK15")
 
         # store AK15 genjets
-        self.VectorRecoCand.extend (['ak15GenJetsNoNu(GenJetsAK15)'])
+        if self.geninfo:
+	        self.VectorRecoCand.extend(['ak15GenJetsNoNu(GenJetsAK15)'])
 
     ## ----------------------------------------------------------------------------------------------
     ## Jet constituents
