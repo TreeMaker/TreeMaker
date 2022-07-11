ad9f89be07bcda31aadb0fdec95afc1195a4477d
Kevin Pedro <kpedro88@gmail.com>
2022-07-11 17:33:50 -0500

    split up ttbar variation dicts to make job assignments easier

---
+++ Production/test/condorSub/dict_Summer20UL16_ttbarvar_had.py
@@ -0,0 +1,12 @@
+flist = {
+
+   "scenario": "Summer20UL16",
+   "args": "saveMinimalGenParticles=False boostedsemivisible=True",
+   "samples": [
+       ['Summer20UL16.TTToHadronic_TuneCP5_erdON_13TeV-powheg-pythia8'],
+       ['Summer20UL16.TTToHadronic_TuneCP5down_13TeV-powheg-pythia8'],
+       ['Summer20UL16.TTToHadronic_TuneCP5up_13TeV-powheg-pythia8'],
+       ['Summer20UL16.TTToHadronic_hdampDOWN_TuneCP5_13TeV-powheg-pythia8'],
+       ['Summer20UL16.TTToHadronic_hdampUP_TuneCP5_13TeV-powheg-pythia8'],
+   ]
+}
