ac5e19461ff77bddae9ef64065d4d741f463ce55
Charlie Kapsiak <kapsi002@umn.edu>
2022-09-30 09:09:37 -0500

    Adding missing file

---
+++ Utils/python/pileupjetid_cfi.py
@@ -0,0 +1,14 @@
+from RecoJets.JetProducers.PileupJetID_cfi import pileupJetId
+from RecoJets.JetProducers.PileupJetID_cfi import _chsalgos_106X_UL16
+from RecoJets.JetProducers.PileupJetID_cfi import _chsalgos_106X_UL16APV
+from RecoJets.JetProducers.PileupJetID_cfi import _chsalgos_106X_UL17
+from RecoJets.JetProducers.PileupJetID_cfi import _chsalgos_106X_UL18
+import FWCore.ParameterSet.Config as cms
+
+
+pileupjetalgos={
+        "2016UL" : _chsalgos_106X_UL16,
+        "2016UL_APV" : _chsalgos_106X_UL16APV,
+        "2017UL" : _chsalgos_106X_UL17,
+        "2018UL" : _chsalgos_106X_UL18
+        }
