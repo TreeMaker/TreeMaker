8b3c890468660a31d5a48c6cc3dcbae868823d4f
Kevin Pedro <kpedro88@gmail.com>
2022-10-20 19:23:43 -0500

    some data versions changed, update file lists and dicts

--- Production/test/condorSub/dict_UL2016B_HIPM_jetht.py
+++ Production/test/condorSub/dict_UL2016B_HIPM_jetht.py
@@ -2,6 +2,6 @@ flist = {
     "scenario": "Summer20UL16APV_DATA",
     "args": "boostedsemivisible=True emerging=True tchannel=True",
     "samples": [
-        ["Run2016B-UL2016_HIPM-ver2-v3.JetHT"],
+        ["Run2016B-UL2016_HIPM-ver2-v2.JetHT"],
    ]
 }
--- Production/test/condorSub/dict_UL2016C_HIPM_singlephoton.py
+++ Production/test/condorSub/dict_UL2016C_HIPM_singlephoton.py
@@ -2,6 +2,6 @@ flist = {
     "scenario": "Summer20UL16APV_DATA",
     "args": "emerging=True",
     "samples": [
-        ["Run2016C-UL2016_HIPM-v2.SinglePhoton"],
+        ["Run2016C-UL2016_HIPM-v4.SinglePhoton"],
    ]
 }
