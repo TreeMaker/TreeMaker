f5e2adc50b6e810dec30f39d1047cbf5f5232e8c
Kevin Pedro <kpedro88@gmail.com>
2023-03-09 23:29:08 -0600

    add two new samples

--- Production/test/condorSub/dict_Fall17Fast_pmssm.py
+++ Production/test/condorSub/dict_Fall17Fast_pmssm.py
@@ -8,5 +8,6 @@ flist = {
         ['Fall17Fast.PMSSM_set_2_LL_1_TuneCP2_13TeV-pythia8'],
         ['Fall17Fast.PMSSM_set_2_LL_2_TuneCP2_13TeV-pythia8'],
         ['Fall17Fast.PMSSM_set_2_prompt_1_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_2_prompt_2_TuneCP2_13TeV-pythia8'],
     ]
 }
