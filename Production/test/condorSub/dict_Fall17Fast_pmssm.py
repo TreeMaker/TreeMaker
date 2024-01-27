6e633a2f8315812738e402fe1f8697037da006ca
Kevin Pedro <kpedro88@gmail.com>
2024-01-27 12:03:06 -0600

    backport pmssm samples

---
+++ Production/test/condorSub/dict_Fall17Fast_pmssm.py
@@ -0,0 +1,13 @@
+flist = {
+    "scenario": "Fall17Fastsig",
+    "samples": [
+        ['Fall17Fast.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_1_prompt_1_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_1_prompt_2_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_1_prompt_3_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_2_LL_1_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_2_LL_2_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_2_prompt_1_TuneCP2_13TeV-pythia8'],
+        ['Fall17Fast.PMSSM_set_2_prompt_2_TuneCP2_13TeV-pythia8'],
+    ]
+}
