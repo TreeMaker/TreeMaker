70bc7633522bd4916e503de21fbb43586e8a0057
Alpana Alpana <km.alpana@cern.ch>
2024-03-22 09:53:49 -0500

    removing wrong config for Summer20UL16APV_ZtoLLgjets

--- Production/test/condorSub/dict_Summer20UL16APV_ZtoLLgjets.py
+++ Production/test/condorSub/dict_Summer20UL16APV_ZtoLLgjets.py
@@ -4,6 +4,5 @@ flist = {
    "samples": [
 
        ['Summer20UL16APV.ZLLGJets_MonoPhoton_PtG-130_TuneCP5_13TeV-amcatnloFXFX-pythia8'],
-       ['Summer20UL16APV.ZLLGJets_MonoPhoton_PtG-15to130_TuneCP5_13TeV-amcatnloFXFX-pythia8'],
    ]
 }
